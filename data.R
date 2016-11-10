ff6372198ce3e9bf01b9d3a080bc0cce47194616
barbarian1803 <bharata180@gmail.com>
2016-11-10 14:18:15 +0900

    new data

--- data.R
+++ data.R
@@ -17,25 +17,24 @@ loadData <- function(dir){
 data <- loadData("data")
 
 setdiff(data[[681]]$code,metadata$Kode)
-View(data[[681]][data[[681]]$code%in%metadata$Kode,])
-
-n <- c()
-for(i in 1:length(data)){
-  n <- c(n,nrow(data[[i]]))
-}
-plot(n)
-
-View(data[[1]])
 mergedAll = Reduce(function(...) merge(..., by="code",all=TRUE), data)
 mergedAll[is.na(mergedAll)] <- 0
 row.names(mergedAll)<-mergedAll$code
 mergedAll <- mergedAll[,-c(1)]
-plot(mergedAll[1,])
+
+calculateCDiff <- function(strt,end){
+  return(100*((end-strt)/strt))
+}
 
 diffData <- mergedAll[,-c(1)]
+
 for(i in 1:nrow(diffData)){
-  for(j in 1:ncol(diffData)){
-    diffData[i,j] <- 100*((mergedAll[i,j+1]-mergedAll[i,j])/mergedAll[i,j])
+  for(j in 1:(ncol(diffData))){
+    diffData[i,j] <- calculateCDiff(mergedAll[i,j],mergedAll[i,j+1])
   }
 }
-diffData[is.na(diffData)] <- 0
+
+
+showPlot<-function(name,start,end){
+  plot(t(mergedAll[name,start:end]),type="l")
+}
